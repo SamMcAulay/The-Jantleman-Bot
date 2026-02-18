e224c9450567f6e25dbf1d3aca044dce20e7a020
SamMcAulay <SamMcAulay@pm.me>
2026-02-18 14:29:51 +0000

    fixed missing import

--- cogs/reputation.py
+++ cogs/reputation.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 import database
 import aiosqlite
+import asyncio
 from datetime import datetime, timedelta
 
 class Reputation(commands.Cog):
